39bcb5e10baad877417e3f0617bc23aea23f1fb0
mikejke <mikejke99@gmail.com>
2022-10-17 22:47:25 +0300

    feat: deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mikejke.github.io',
+  base: '/turbo-octo-disco',
   integrations: [vue()],
   vite: {
     plugins: [eslint()],
